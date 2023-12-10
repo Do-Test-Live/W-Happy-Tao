b250beee5e282ed04f09da3d36747da9ff90b416
Monoget <monoget2@gmail.com>
2023-12-10 17:38:14 +0600

    update all

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+let scrollToTop = document.getElementById("ht-scroll-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTop.style.display = "block";
+    } else {
+        scrollToTop.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
